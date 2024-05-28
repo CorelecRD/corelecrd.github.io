84ae2ee79ac8ed94fcdd5be5e62e0b1585f4cc5a
CorelecRD <lorenzo.leijnen@corelec.eu>
2024-05-28 09:10:36 +0200

    Add files via upload

--- akeron.js
+++ akeron.js
@@ -23,6 +23,8 @@ const akeron = {
 	CapteurSel: null,
 	CapteurTemp: null,
 	PompesForcees: null,
+	RawFieldM13: null,
+	RawFieldA10: null,
 
 	// Etat des relais
 	PompePlusActive: null,
@@ -78,6 +80,7 @@ function parseData(buf, appareil) {
 				appareil.VoletActif = byteToBool(buf[10], 4);
 				appareil.VoletForce = byteToBool(buf[10], 3);
 				appareil.FlowSwitch = byteToBool(buf[10], 2);
+				appareil.RawFieldA10 = buf[10];
 				
 				appareil.AlarmeElx = buf[12] & 0x0F;
 				
@@ -109,130 +112,135 @@ function composeRequest(mnemonic) {
 }
 
 function compositionConsigne(donnee, typemesure) {
-    let consigne;
-    const trame = trameDonneesVide('S');
+	let consigne;
+	const trame = trameDonneesVide('S');
 
-    switch (typemesure) {
-        case 'ph':
-            trame[1] = 'S'.charCodeAt(0);
-            consigne = donnee;
-            trame[2] = highByte(consigne);
-            trame[3] = lowByte(consigne);
-            break;
-        case 'redox':
-            trame[1] = 'E'.charCodeAt(0);
-            consigne = Math.round(donnee);
-            trame[2] = highByte(consigne);
-            trame[3] = lowByte(consigne);
-            break;
-    }
+	switch (typemesure) {
+		case 'ph':
+			trame[1] = 'S'.charCodeAt(0);
+			consigne = donnee;
+			trame[2] = highByte(consigne);
+			trame[3] = lowByte(consigne);
+			break;
+		case 'redox':
+			trame[1] = 'E'.charCodeAt(0);
+			consigne = Math.round(donnee);
+			trame[2] = highByte(consigne);
+			trame[3] = lowByte(consigne);
+			break;
+		default:
+			break;
+	}
 	trame[15] = get_crc(trame.slice(0, 15)); // CRC
 
-    return trame;
+	return trame;
 }
 
 function compositionOffset(donnee, typemesure) {
-    let offset;
-    const trame = trameDonneesVide('M');
+	let offset;
+	const trame = trameDonneesVide('M');
 
-    switch (typemesure) {
-        case 'ph':
-            offset = Math.round(donnee * 100);
-            trame[2] = highByte(offset);
-            trame[3] = lowByte(offset);
-            break;
-        case 'redox':
-            offset = Math.round(donnee);
-            trame[4] = highByte(offset);
-            trame[5] = lowByte(offset);
-            break;
-        case 'temp':
-            offset = Math.round(donnee * 10);
-            trame[6] = highByte(offset);
-            trame[7] = lowByte(offset);
-            break;
-        case 'sel':
-            offset = Math.round(donnee * 10);
-            trame[8] = highByte(offset);
-            trame[9] = lowByte(offset);
-            break;
-    }
+	switch (typemesure) {
+		case 'ph':
+			offset = Math.round(donnee * 100);
+			trame[2] = highByte(offset);
+			trame[3] = lowByte(offset);
+			break;
+		case 'redox':
+			offset = Math.round(donnee);
+			trame[4] = highByte(offset);
+			trame[5] = lowByte(offset);
+			break;
+		case 'temp':
+			offset = Math.round(donnee * 10);
+			trame[6] = highByte(offset);
+			trame[7] = lowByte(offset);
+			break;
+		case 'sel':
+			offset = Math.round(donnee * 10);
+			trame[8] = highByte(offset);
+			trame[9] = lowByte(offset);
+			break;
+		default:
+			break;
+	}
 	trame[15] = get_crc(trame.slice(0, 15)); // CRC
 
-    return trame;
+	return trame;
 }
 
 function compositionSeuils(seuils, typemesure) {
-    let seuilh, seuilb;
-    let trame = trameDonneesVide('D');
-    if (typemesure === 'ph') trame = trameDonneesVide('S');
+	let seuilh, seuilb;
+	let trame = trameDonneesVide('D');
+	if (typemesure === 'ph') trame = trameDonneesVide('S');
 
-    switch (typemesure) {
-        case 'temp':
-            trame[4] = seuils.Erreurs.Max;
-            trame[5] = seuils.Erreurs.Min;
-            trame[6] = seuils.Warnings.Max;
-            trame[7] = seuils.Warnings.Min;
-            break;
-        case 'sel':
-            trame[8] = seuils.Warnings.Min * 10;
-            trame[9] = seuils.Erreurs.Min * 10;
-            break;
-        case 'ph':
-            seuilh = Math.round(seuils.Erreurs.Max * 100);
-            seuilb = Math.round(seuils.Erreurs.Min * 100);
-            trame[10] = highByte(seuilh);
-            trame[11] = lowByte(seuilh);
-            trame[12] = highByte(seuilb);
-            trame[13] = lowByte(seuilb);
-            break;
-    }
+	switch (typemesure) {
+		case 'temp':
+			trame[4] = seuils.Erreurs.Max;
+			trame[5] = seuils.Erreurs.Min;
+			trame[6] = seuils.Warnings.Max;
+			trame[7] = seuils.Warnings.Min;
+			break;
+		case 'sel':
+			trame[8] = seuils.Warnings.Min * 10;
+			trame[9] = seuils.Erreurs.Min * 10;
+			break;
+		case 'ph':
+			seuilh = Math.round(seuils.Erreurs.Max * 100);
+			seuilb = Math.round(seuils.Erreurs.Min * 100);
+			trame[10] = highByte(seuilh);
+			trame[11] = lowByte(seuilh);
+			trame[12] = highByte(seuilb);
+			trame[13] = lowByte(seuilb);
+			break;
+		default:
+			break;
+	}
 	trame[15] = get_crc(trame.slice(0, 15)); // CRC
 
-    return trame;
+	return trame;
 }
 
 function compositionValeurVolet(volet) {
-    const trame = trameDonneesVide('A');
-    trame[9] = volet;
+	const trame = trameDonneesVide('A');
+	trame[9] = volet;
 	trame[15] = get_crc(trame.slice(0, 15)); // CRC
 
-    return trame;
+	return trame;
 }
 
 function compositionActivationBoost(boost) {
-    const trame = trameDonneesVide('A');
-    trame[3] = highByte(boost);
-    trame[4] = lowByte(boost);
+	const trame = trameDonneesVide('A');
+	trame[3] = highByte(boost);
+	trame[4] = lowByte(boost);
 	trame[15] = get_crc(trame.slice(0, 15)); // CRC
 
-    return trame;
+	return trame;
 }
 
-function compositionActivationVolet(appareil, volet) {
-    const trame = trameDonneesVide('A');
-    trame[10] = byteSet(volet, 3, 0);
+function compositionActivationVolet(appareil) {
+	const trame = trameDonneesVide('A');
+	trame[10] = byteSet(!appareil.VoletActif, 3, appareil.RawFieldA10);
 	trame[15] = get_crc(trame.slice(0, 15)); // CRC
 
-    return trame;
+	return trame;
 }
 
 function compositionProduction(production) {
-    const trame = trameDonneesVide('A');
-    trame[2] = production;
+	const trame = trameDonneesVide('A');
+	trame[2] = production;
 	trame[15] = get_crc(trame.slice(0, 15)); // CRC
 
-    return trame;
+	return trame;
 }
 
 function compositionResetUsine() {
-    const trame = trameDonneesVide('M');
-    trame[14] = 0xCF;
+	const trame = trameDonneesVide('M');
+	trame[14] = 0xCF;
 	trame[15] = get_crc(trame.slice(0, 15)); // CRC
 	
-    return trame;
+	return trame;
 }
-
 
 
 //-------------------------------//
@@ -245,19 +253,19 @@ function byteToBool(cible, bit) {
 }
 
 function byteSet(condition, position, value) {
-    if (condition) {
-        return value | (1 << position);
-    } else {
-        return value & ~(1 << position);
-    }
+	if (condition) {
+		return value | (1 << position);
+	} else {
+		return value & ~(1 << position);
+	}
 }
 
 function highByte(value) {
-    return (value >> 8) & 0xFF;
+	return (value >> 8) & 0xFF;
 }
 
 function lowByte(value) {
-    return value & 0xFF;
+	return value & 0xFF;
 }
 
 function get_crc(array) {
@@ -269,15 +277,15 @@ function get_crc(array) {
 }
 
 function trameDonneesVide(mnemo) {
-    const bArray = new Uint8Array(17);
-    bArray[0] = 0x2A; // Start byte
-    bArray[1] = mnemo.charCodeAt(0); // Convert character to its byte representation
+	const bArray = new Uint8Array(17);
+	bArray[0] = 0x2A; // Start byte
+	bArray[1] = mnemo.charCodeAt(0); // Convert character to its byte representation
 
-    for (let i = 2; i < 16; i++) {
-        bArray[i] = 0xFF; // Fill with 0xFF
-    }
+	for (let i = 2; i < 16; i++) {
+		bArray[i] = 0xFF; // Fill with 0xFF
+	}
 
-    bArray[16] = 0x2A; // End byte
+	bArray[16] = 0x2A; // End byte
 
-    return bArray;
+	return bArray;
 }
